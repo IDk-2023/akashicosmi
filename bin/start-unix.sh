05192496d3e98a421af1f6d5aed449baa748e79f
ElChi <136360907+IDk-2023@users.noreply.github.com>
2025-04-06 01:41:01 +0300

    Update start-unix.sh

--- bin/start-unix.sh
+++ bin/start-unix.sh
@@ -5,6 +5,7 @@ set -euxo pipefail
 while [ true ]
 do
 	echo "Starting up server..."
+ 	sudo iptables -A INPUT -p tcp --dport 27017 -j ACCEPT
 	cd akashicosmi/bin
  	./akashi &
 	echo "Server has shut down. Will restart in 2 seconds (use CTRL-C to cancel)"
