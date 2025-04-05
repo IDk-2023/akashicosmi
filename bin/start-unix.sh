fc08192ae6744e686482eba88a038272a5ff15bb
ElChi <136360907+IDk-2023@users.noreply.github.com>
2025-04-06 00:24:18 +0300

    Update start-unix.sh

--- bin/start-unix.sh
+++ bin/start-unix.sh
@@ -6,7 +6,7 @@ while [ true ]
 do
 	echo "Starting up server..."
 	cd akashicosmi/bin
- 	./akashi
+ 	./akashi &
 	echo "Server has shut down. Will restart in 2 seconds (use CTRL-C to cancel)"
 	sleep 2
 done
